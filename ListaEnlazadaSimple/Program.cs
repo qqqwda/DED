f8d52d57f4f1cad7cf34c9ab68c00037d09f2068
Daniel <cursedelboom@gmail.com>
2019-06-01 16:44:14 -0400

    Creation project
    Creation

---
+++ ListaEnlazadaSimple/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListaEnlazadaSimple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
